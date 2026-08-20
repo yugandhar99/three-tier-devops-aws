75f835a7472f15adf74401e771344e64f4eddbe3
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-08-19 20:34:38 -0500

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,7 @@
 output "url" {
   value = "https://${var.frontend_domains[0]}"
 }
- 
+
 output "ecs_cluster_name" {
   description = "Name of the ECS cluster for backend deployment"
   value       = module.backend.ecs_cluster_name
